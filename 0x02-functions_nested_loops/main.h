6c5db5deef301ca99056e71704faa8cbafd0d6b8
sunnywizy <sundayobasi222@gmail.com>
2023-08-15 16:06:03 +0100

    Working on the nexted loop

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: holberton.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops directory.
- */
+
 
 int _putchar(char c);
 void print_alphabet(void);
