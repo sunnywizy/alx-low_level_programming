9cd393a32116716da1ee7e1abb0b33b20c1f0010
sunnywizy <sundayobasi222@gmail.com>
2023-08-24 23:50:37 +0100

    Task for pointer, arrays and strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,42 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
 
-char *rot13(char *input) {
-    char *output = (char *)malloc(strlen(input) + 1);
-    if (output == NULL) {
-        return NULL;
-    }
+char *rot13(char *s)
+{
+	int count = 0, i;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-    char *input_ptr = input;
-    char *output_ptr = output;
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + count) == alphabet[i])
+			{
+				*(s + count) = rot13[i];
+				break;
+			}
+		}
+		count++;
+	}
 
-    for (; *input_ptr != '\0'; input_ptr++, output_ptr++) {
-        if ((*input_ptr >= 'A' && *input_ptr <= 'Z')) {
-            *output_ptr = ((*input_ptr - 'A' + 13) % 26) + 'A';
-        } else if ((*input_ptr >= 'a' && *input_ptr <= 'z')) {
-            *output_ptr = ((*input_ptr - 'a' + 13) % 26) + 'a';
-        } else {
-            *output_ptr = *input_ptr;
-        }
-    }
-    *output_ptr = '\0';
-
-    return output;
-}
-
-int main() {
-    char input[] = "Hello, World!";
-    char *encoded = rot13(input);
-    
-    if (encoded) {
-        printf("Original: %s\n", input);
-        printf("Encoded : %s\n", encoded);
-        free(encoded);
-    } else {
-        printf("Memory allocation failed.\n");
-    }
-
-    return 0;
+	return (s);
 }
