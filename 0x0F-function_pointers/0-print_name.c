7b9192847a6d6c230b34a53a57ff1b53f2bd3dd5
sunnywizy <sundayobasi222@gmail.com>
2023-09-13 17:53:17 +0100

    The function pointers task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,6 +8,8 @@
  * Return: no return.
 */
 void print_name(char *name, void (*f)(char *)) {
-    if (name && f)
-            f(name);
+    if (name == NULL || f == NULL)
+            return;
+    
+    f(name);
 }
