b17066e11e95f37a1a215cdc89d56f742b27b286
sunnywizy <sundayobasi222@gmail.com>
2023-09-13 18:08:35 +0100

    The function pointer task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,12 @@
 /**
  * print_name - prints a name.
  * @name: input name.
- * @f: function pointer
- * 
+ * @f: function pointer.
+ *
  * Return: no return.
-*/
-void print_name(char *name, void (*f)(char *)) {
-    if (name == NULL || f == NULL)
-            return;
-    
-    f(name);
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
 }
