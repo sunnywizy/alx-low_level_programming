51588535cd3e3f0c36b9ad58a994a19463a3e388
sunnywizy <sundayobasi222@gmail.com>
2023-09-14 16:11:46 +0100

    The function pointers task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    char *opc = (char *) main;
-    int i, nbytes;
-    
-    if(argc != 2) {
-        printf("Error\n");
-        exit(1);
-    }
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int i, nbytes;
 
-    nbytes = atoi(argv[1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    if(nbytes < 0) {
-        printf("Error\n");
-        exit(2);
-    }
+	nbytes = atoi(argv[1]);
 
-    for(i = 0; i < nbytes; i++) {
-        printf("%02x", opc[i] & 0xFF);
-        if(i != nbytes -1) {
-            printf(" ");
-        }
-    }
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    printf("\n");
-    return(0);
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
 }
