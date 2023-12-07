c155ae55d619b7454fb76e58abb69acf9dfa92a8
sunnywizy <sundayobasi222@gmail.com>
2023-12-07 16:05:36 +0100

    I just created some files

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+size_t dlistint_len(const dlistint_t *h)
+{
+        size_t nodes = 0;
+
+        while (h)
+        {
+                nodes++;
+                h = h->next;
+        }
+
+        return (nodes);
+}
