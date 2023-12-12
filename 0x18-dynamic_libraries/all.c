758f3c9ae59109335dfe32966cc0cecd09e95e87
sunnywizy <sundayobasi222@gmail.com>
2023-12-12 15:43:53 +0100

    I just create a 100 operation.so file

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/* add - To add two integer
+ * a - The fist integer
+ * b - The second integer
+ * return - both integer
+ */
+int add(int a, int b)
+{
+    return (a + b);
+}
+
+/* sub - To sub two integer
+ * a - The fist integer
+ * b - The second integer
+ * return - both integer
+ */
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/* mul - To mul two integer
+ * a - The fist integer
+ * b - The second integer
+ * return - both integer
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/* div - To div two integer
+ * a - The fist integer
+ * b - The second integer
+ * return - both integer
+ */
+int div(int a, int b)
+{
+    return (a / b);
+}
+
+/* add - To add two integer
+ * a - The fist integer
+ * b - The second integer
+ * return - both integer
+ */
+int mol(int a, int b)
+{
+    return (a % b);
+}
